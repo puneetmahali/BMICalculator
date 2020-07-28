2f642c481f57a608e542cdc987b76595f5dc0c8c
Puneet Mahali <iospuneet@gmail.com>
2020-07-28 23:00:40 +0200

    initial Commit

---
+++ BMICalculator/BMICalculator/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Puneet Mahali on 28.07.20.
+//  Copyright © 2020 Puneet Mahali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
