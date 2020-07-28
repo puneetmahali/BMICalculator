496d32cd8ace4ebbc10625fce0830eaa90328fde
Puneet Mahali <iospuneet@gmail.com>
2020-07-28 23:07:13 +0200

    finished project

---
+++ BMICalculator/BMICalculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMICalculator
+//
+//  Created by Puneet Mahali on 28.07.20.
+//  Copyright © 2020 Puneet Mahali. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
